195fe3de2c047fa3586c1b1d221605f9d43b71c9
mad-hin <46859133+mad-hin@users.noreply.github.com>
2020-07-19 12:28:43 +0800

    whatever

--- editor.c
+++ editor.c
@@ -1,13 +1,20 @@
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
-    void
-    enableRawMode()
+struct termios orig_termios;
+void disableRawMode()
 {
-  struct termios raw;
-  tcgetattr(STDIN_FILENO, &raw);
+  tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+void enableRawMode()
+{
+  tcgetattr(STDIN_FILENO, &orig_termios);
+  atexit(disableRawMode);
+  struct termios raw = orig_termios;
   raw.c_lflag &= ~(ECHO);
   tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
+
 int main()
 {
   enableRawMode();
