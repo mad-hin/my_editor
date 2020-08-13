fe37f96c857036ef09ef11efe97b26cb4b08c698
mad-hin <46859133+mad-hin@users.noreply.github.com>
2020-08-13 21:48:18 +0800

    update 13/08/2020

--- editor.c
+++ editor.c
@@ -10,6 +10,19 @@
 #define ABUF_INIT {NULL, 0}
 #define EDITOR_VERSION "0.0.1"
 
+enum editorKey
+{
+	ARROW_LEFT = 1000,
+	ARROW_RIGHT,
+	ARROW_UP,
+	ARROW_DOWN,
+	DEL_KEY,
+	HOME_KEY,
+	END_KEY,
+	PAGE_UP,
+	PAGE_DOWN
+};
+
 /***User Defined Data Types ***/
 struct editorConfig
 {
@@ -38,9 +51,10 @@ void abFree(struct abuf *ab);
 void die(const char *s);
 void disableRawMode();
 void enableRawMode();
-char editorReadKey();
+int editorReadKey();
 void editorProcessKeypress();
 void editorRefreshScreen();
+void editorMoveCursor(int key);
 void editorDrawRows(struct abuf *ab);
 void initEditor();
 int getCursorPosition(int *rows, int *cols);
@@ -138,7 +152,7 @@ int getCursorPosition(int *rows, int *cols)
 
 /*** input ***/
 // Get character from standard input
-char editorReadKey()
+int editorReadKey()
 {
 	int nread;
 	char c;
@@ -147,19 +161,143 @@ char editorReadKey()
 		if (nread == -1 && errno != EAGAIN) // EAGAIN = "there is no data available right now, try again later"
 			die("read");
 	}
-	return c;
+
+	if (c == '\x1b')
+	{
+		char seq[3];
+		if (read(STDIN_FILENO, &seq[0], 1) != 1)
+			return '\x1b';
+		if (read(STDIN_FILENO, &seq[1], 1) != 1)
+			return '\x1b';
+		if (seq[0] == '[')
+		{
+			if (seq[1] >= '0' && seq[1] <= '9')
+			{
+				if (read(STDIN_FILENO, &seq[2], 1) != 1)
+					return '\x1b';
+				if (seq[2] == '~')
+				{
+					switch (seq[1])
+					{
+					case '1':
+						return HOME_KEY;
+					case '3':
+						return DEL_KEY;
+					case '4':
+						return END_KEY;
+					case '5':
+						return PAGE_UP;
+					case '6':
+						return PAGE_DOWN;
+					case '7':
+						return HOME_KEY;
+					case '8':
+						return END_KEY;
+					}
+				}
+			}
+			else
+			{
+				switch (seq[1])
+				{
+				case 'A':
+					return ARROW_UP;
+				case 'B':
+					return ARROW_DOWN;
+				case 'C':
+					return ARROW_RIGHT;
+				case 'D':
+					return ARROW_LEFT;
+				case 'H':
+					return HOME_KEY;
+				case 'F':
+					return END_KEY;
+				}
+			}
+		}
+		else if (seq[0] == 'O')
+		{
+			switch (seq[1])
+			{
+			case 'H':
+				return HOME_KEY;
+			case 'F':
+				return END_KEY;
+			}
+		}
+
+		return '\x1b';
+	}
+	else
+	{
+		return c;
+	}
 }
 /*** input ***/
 void editorProcessKeypress()
 {
-	char c = editorReadKey();
+	int c = editorReadKey();
 	switch (c)
 	{
+	case HOME_KEY:
+		E.cx = 0;
+		break;
+	case END_KEY:
+		E.cx = E.screencols - 1;
+		break;
+
 	case CTRL_KEY('q'): // ctrl + q to quit
 		// x1b == "esc" in ASCII
 		write(STDOUT_FILENO, "\x1b[2J", 4); // \x1b[2J is the code for clearing the screen and set cursor to home
 		write(STDOUT_FILENO, "\x1b[H", 3);	// \x1b[H is the code for returning the cursor to the home position
 		exit(0);
+		break;
+
+	case PAGE_UP:
+	case PAGE_DOWN:
+	{
+		int times = E.screenrows;
+		while (times--)
+			editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+	}
+	break;
+	
+	case ARROW_UP:
+	case ARROW_DOWN:
+	case ARROW_LEFT:
+	case ARROW_RIGHT:
+		editorMoveCursor(c);
+		break;
+	}
+}
+
+void editorMoveCursor(int key)
+{
+	switch (key)
+	{
+	case ARROW_LEFT:
+		if (E.cx != 0)
+		{
+			E.cx--;
+		}
+		break;
+	case ARROW_RIGHT:
+		if (E.cx != E.screencols - 1)
+		{
+			E.cx++;
+		}
+		break;
+	case ARROW_UP:
+		if (E.cy != 0)
+		{
+			E.cy--;
+		}
+		break;
+	case ARROW_DOWN:
+		if (E.cy != E.screenrows - 1)
+		{
+			E.cy++;
+		}
 		break;
 	}
 }
@@ -246,7 +384,7 @@ void editorDrawRows(struct abuf *ab)
 
 		if (y < E.screenrows - 1)
 		{
-			abAppend(ab, "\r\n", 1);
+			abAppend(ab, "\r\n", 2);
 		}
 	}
 }
